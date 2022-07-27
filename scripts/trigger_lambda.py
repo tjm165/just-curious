4137c149cd124b17ddfaa577518d1dad0f944bfb
Thomas M <tjm165@case.edu>
2022-07-26 23:15:52 -0500

    add new endpoints

--- scripts/trigger_lambda.py
+++ scripts/trigger_lambda.py
@@ -7,7 +7,7 @@ iterations = int(sys.argv[1])
 resp_codes = collections.defaultdict(lambda: 0)
 
 for i in range(iterations):
-    url = "https://8adf26xf06.execute-api.us-east-2.amazonaws.com/python_lambda"
+    url = "https://ufsjj3gw67.execute-api.us-east-2.amazonaws.com/python1"
     resp = requests.get(url)
     resp_codes[resp.status_code] += 1
     print(i)
