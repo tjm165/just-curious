bd36eea8a2c768d08ffe54378117ca0fcbc20f35
Thomas M <tjm165@case.edu>
2022-07-29 00:36:17 -0500

    readme

--- scripts/add_a_line.py
+++ scripts/add_a_line.py
@@ -6,14 +6,14 @@ lines_to_add = int(sys.argv[1])
 
 
 #############
-# for _ in range(lines_to_add):
-#     path_to_file = "./infa/src/project/lambda1/python/lambda.py"
-#     with open(path_to_file, "r") as f:
-#         contents = f.readlines()
-#     contents.insert(17, string_to_add)
-#     with open(path_to_file, "w") as f:
-#         contents = "".join(contents)
-#         f.write(contents)
+for _ in range(lines_to_add):
+    path_to_file = "./infa/src/project/lambda1/python/lambda.py"
+    with open(path_to_file, "r") as f:
+        contents = f.readlines()
+    contents.insert(17, string_to_add)
+    with open(path_to_file, "w") as f:
+        contents = "".join(contents)
+        f.write(contents)
 
 #############
 for _ in range(lines_to_add):
