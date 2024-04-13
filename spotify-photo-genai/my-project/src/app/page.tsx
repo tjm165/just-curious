c75530355095bcb7b04d46a0c93a61b92336266b
Thomas M <tjm165@case.edu>
2024-04-13 13:34:13 -0500

    Cleanup UI

--- spotify-photo-genai/my-project/src/app/page.tsx
+++ spotify-photo-genai/my-project/src/app/page.tsx
@@ -1,13 +1,10 @@
-import Image from "next/image";
 import { SpotifySuggestions } from "@/components/spotify-suggestions";
-
-import { SpotifySuggestions2 } from "@/components/spotify-suggestions2";
 
 export default function Home() {
   return (
     <>
       <SpotifySuggestions />
-      <SpotifySuggestions2 />
+      {/* <SpotifySuggestions2 /> */}
     </>
   );
 }
